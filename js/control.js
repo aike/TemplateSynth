2588a114d68fec3a166ebc39c2962c544e8db6db
aike <aaiikkee@gmail.com>
2014-09-09 15:56:59 +0900

    fix knob bug

--- js/control.js
+++ js/control.js
@@ -13,10 +13,6 @@ $(function() {
 	elem_id = "#mvol";
 	synth.controls.push(elem_id);
 	$(elem_id)
-	.attr({
-		value: 50,
-		defvalue: 50
-	})
 	.change(function(e) {
 		synth.setParam(synth.param.MasterVolume, e.target.value);
 		//console.log($('#mvol')[0].value);
